013f3a649af77f6b63cc589990515bd353859328
Jhonathan Araujo <jonathan.reboucas@unisys.com>
2019-12-26 15:03:39 -0300

    Testando threads em C#

---
+++ TestandoThread/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TestandoThread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pgbThreads = new System.Windows.Forms.ProgressBar();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pgbThreads
+            // 
+            this.pgbThreads.Location = new System.Drawing.Point(13, 86);
+            this.pgbThreads.Maximum = 500;
+            this.pgbThreads.Name = "pgbThreads";
+            this.pgbThreads.Size = new System.Drawing.Size(289, 23);
+            this.pgbThreads.TabIndex = 0;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(123, 40);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 1;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 131);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.pgbThreads);
+            this.Name = "Form1";
+            this.Text = "Testando Threads";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pgbThreads;
+        private System.Windows.Forms.Button btnIniciar;
+    }
+}
+
---
+++ TestandoThread/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestandoThread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Esta é a thread principal do programa");
+        }
+
+        private void ThreadTarefa()
+        {
+            int step, novoValor = 0;
+            Random rnd = new Random();
+
+            while (novoValor < pgbThreads.Maximum)
+            {
+                if (pgbThreads.Value == 0)
+                    step = pgbThreads.Step + rnd.Next(0, 15);
+                else
+                    step = pgbThreads.Step + rnd.Next(-25, 15);
+
+                novoValor = pgbThreads.Value + step;
+
+                if (novoValor > pgbThreads.Maximum)
+                {
+                    novoValor = pgbThreads.Maximum;
+                    SetControlPropertyValue(pgbThreads, "value", novoValor);
+                }
+                else
+                    SetControlPropertyValue(pgbThreads, "value", novoValor);
+
+                Thread.Sleep(100);
+            }
+
+            MessageBox.Show("Thread concluída");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread trd = new Thread(new ThreadStart(ThreadTarefa));
+            trd.IsBackground = true;
+            trd.Start();
+        }
+
+        delegate void SetControlValueCallback(Control oControl, string propName, object propValue);
+        private void SetControlPropertyValue(Control oControl, string propName, object propValue)
+        {
+            if (oControl.InvokeRequired)
+            {
+                SetControlValueCallback d = new SetControlValueCallback(SetControlPropertyValue);
+                oControl.Invoke(d, new object[] { oControl, propName, propValue });
+            }
+            else
+            {
+                Type t = oControl.GetType();
+                PropertyInfo[] props = t.GetProperties();
+
+                foreach (PropertyInfo p in props)
+                {
+                    if (p.Name.ToUpper() == propName.ToUpper())
+                    {
+                        p.SetValue(oControl, propValue, null);
+                    }
+                }
+            }
+        }
+    }
+}
